c7f6731b95619518f3647dd4ae33774223dba779
Robbie Campbell <rcampbell182@gmail.com>
2016-10-03 16:55:07 +0100

    initial commit

---
+++ filmApi.js
@@ -0,0 +1,32 @@
+var Films = require("./client/src/models/films");
+
+var FilmApi = function(app) {
+  
+  var films = new Films();
+
+  app.get("/films", function( req, res ) {
+    res.json( {data: films} )
+  });
+
+  app.get("/films/:id", function(req, res){
+    res.json({data: films[req.params.id]});
+  });
+
+  app.post("/films", function(req, res) {
+    films.push(req.body.film);
+    res.json({data: films});
+  });
+
+  app.delete("/films/:id", function(req, res) {
+    films.splice( req.params.id, 1)
+    res.json({data: films});
+  });
+
+  app.put("/films/:id", function(req, res) {
+    films[req.params.id] = req.body.film;
+    res.json({data: films});
+  })
+
+}
+
+module.exports = FilmApi;
